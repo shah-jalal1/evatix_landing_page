135719168d0c4502f6226028879c936b3dafd264
shahjalalcse <shah-jalal1>
2021-07-05 17:42:47 +0600

    navbar done'

---
+++ src/components/Hero/Navbar/Navbar.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import logo from '../../../images/logo_white@2x.png'
+
+const Navbar = () => {
+    return (
+        <div class="">
+            {/* <nav className="bg-gray-800"> */}
+            <nav style={{background: 'rgba(250,98,82,255)'}}>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-8  text-gray-300"
+                  src={logo}
+                  alt="Workflow"
+                />
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <a
+                    href="#"
+                    className=" hover:bg-white-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Home
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Portfolio
+                  </a>
+
+                </div>
+              </div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+            <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Reports
+                  </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="md:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <a
+              href="#"
+              className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Home
+            </a>
+
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Portfolio
+            </a>
+
+           
+          </div>
+        </div>
+      </nav>
+        </div>
+    );
+};
+
+export default Navbar;
